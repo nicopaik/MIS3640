b06546d01f5b1cf9eebe0038742dab077f7b3c32
nicopaik <npaiksohn1@babson.edu>
2019-09-10 15:11:31 -0400

    Create calc.py

---
+++ Session01/calc.py
@@ -0,0 +1,25 @@
+print (46+37+38)
+print (2016-1996)
+print (15/3)
+print (2**6)
+
+#Exercise 1
+#1). Syntax Error
+#2). Not Defined Error
+#3). 2++2 = 4
+#4). 02 = Syntax Error
+#5). Invalid Syntax
+
+#Exercise 2
+#1
+seconds = 42 * 60 + 42
+print (seconds)
+
+#2
+miles = 10 * 1.61
+print (miles)
+
+#3
+pace_secs =  (seconds/miles)
+pace_mins = int(pace_secs//60)
+speed_hrs = (pace_mins/60)
