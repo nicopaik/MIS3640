910c9fb6e7a8d84707aaef7890764beaff33722c
nicopaik <npaiksohn1@babson.edu>
2019-09-11 21:02:17 -0400

    Create demo02.py

---
+++ Session02/demo02.py
@@ -0,0 +1,21 @@
+# print('Hello, World!')
+
+print('Hey Jude, don\'t make it bad')
+# Character after \ is still part of the string
+
+print('The total number of medals in Rio 2016 is', 46 + 37 + 38, '.')
+# Separate functions with ,
+
+print('46 + 37 + 38 =', 46 + 37 + 38)
+
+#name = input()
+#print ('hello, ', name)
+#input() enter something from console
+
+#message = 'I did something incredible today!'
+#n = 100
+#pi = 3.14
+
+first_name = 'John'
+last_name = 'Lennon'
+print('Hello,'+ first_name + ' ' + last_name + '.')
