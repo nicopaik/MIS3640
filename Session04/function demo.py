44a0960c87f5c3b988ac9029283c18bbbbb77861
nicopaik <npaiksohn1@babson.edu>
2019-09-17 21:40:10 -0400

    Create function demo.py

---
+++ Session04/function demo.py
@@ -0,0 +1,24 @@
+def print_lyrics():
+    print("Hey Jude. Don't make it bad.")
+    print("Take a sad song and make it better.")
+
+# print_lyrics()
+
+# print(type(print_lyrics))
+
+def repeat_lyrics():
+    print_lyrics()
+    print('Na - na')
+    print_lyrics()
+
+# repeat_lyrics()
+
+def print_twice(name):
+    print(name)
+    print(name)
+
+print_twice('Brian')
+print_twice('Hey, Jude')
+
+his_name = 'Brian'
+print_twice(his_name)
