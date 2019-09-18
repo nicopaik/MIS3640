0ceb8ae03ce3e715e18fad49f3fe1779b837d298
nicopaik <npaiksohn1@babson.edu>
2019-09-17 21:38:40 -0400

    Create myabs.py

---
+++ Session04/myabs.py
@@ -0,0 +1,34 @@
+def my_abs(number):
+    if number >= 0:
+        return number
+    else:
+        return -number
+
+#my_abs(10)
+#my_abs(-10)
+#my_abs(0)
+
+print(my_abs(-4.2))
+
+#def cat_twice(part1, part2):
+ #   cat = part1 + part2
+ #   #print(cat)
+ #   print_twice(cat)
+
+#cat_twice(100, 200)
+
+#cat_twice('100', '200')
+
+def give_me_a_break():
+    msg = 'break'
+    return msg
+
+#print(give_me_a_break())
+
+#Quadratic Formula
+
+a = (a)
+b = (b)
+c = (c)
+
+ax**2 + bx + c = qd
