82c63c0bd2cd762d3d87f6ebbe9a57664326637c
nicopaik <npaiksohn1@babson.edu>
2019-09-24 01:09:14 -0400

    Create mypolygon.py

---
+++ Session05/mypolygon.py
@@ -0,0 +1,29 @@
+import turtle
+brian = turtle.Turtle()
+
+print(brian)
+
+brian.fd(100)
+brian.lt(90)
+brian.fd(100)
+brian.lt(90)
+brian.fd(100)
+brian.lt(90)
+brian.fd(100)
+brian.lt(90)
+
+for i in range(4):
+    brian.fd(100)
+    brian.lt(90)
+
+def square(T, length)
+    for i in range(4):
+        t.fd(length)
+        t.lt(90)
+
+square(brian, 200)
+
+arc(brian, 100, 360)
+move(brian, -200, 100)
+polygon(brian, 3, 100)
+
